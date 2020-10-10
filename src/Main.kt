85d44338254d58331a94c925ca721f6fc6d061d6
Влад Концевых <vladkontsevykh@list.ru>
2020-10-10 14:58:12 +0300

    Operations pt.2
    More foolproof safety for get and set
    Matrix:
    +toString()
    +times(E: Matrix)
    
    Main:
    Lil' test for aforementioned

--- src/Main.kt
+++ src/Main.kt
@@ -1,4 +1,21 @@
 
 fun main(args: Array<String>) {
+ val a: Matrix = Matrix(1,3)
+ val b: Matrix = Matrix(3,3)
 
- }
+ a.set(1,1,1.0)
+ a.set(1,2,2.0)
+ a.set(1,3,3.0)
+
+ b.set(1,1,1.0)
+ b.set(1,2,2.0)
+ b.set(1,3,3.0)
+ b.set(2,1,4.0)
+ b.set(2,2,5.0)
+ b.set(2,3,6.0)
+ b.set(3,1,7.0)
+ b.set(3,2,8.0)
+ b.set(3,3,9.0)
+
+ println(a * b)
+}
--- src/Matrix.kt
+++ src/Matrix.kt
@@ -4,14 +4,14 @@ class Matrix(val rows: Int, val columns: Int) {
     private val matrix: Array<Double> = Array(rows * columns) { 0.0 }
 
     fun get(row: Int, column: Int): Double {
-        if (row <= rows && column <= columns)
+        if (row <= rows && column <= columns && row > 0 && column > 0)
             return matrix[(row - 1) * columns + column - 1]
         else
             throw IllegalArgumentException("get[$row, $column]")
     }
 
     fun set(row: Int, column: Int, number: Double) {
-        if (row <= rows && column <= columns)
+        if (row <= rows && column <= columns && row > 0 && column > 0)
             matrix[(row - 1) * columns + column - 1] = number
         else
             throw IllegalArgumentException("set[$row, $column]")
@@ -39,6 +39,19 @@ class Matrix(val rows: Int, val columns: Int) {
         }
     }
 
+    operator fun times(b: Matrix): Matrix {
+        if (columns == b.rows) {
+            val c = Matrix(rows, b.columns)
+            for (i in 1..rows)
+                for (j in 1..b.columns)
+                    for (k in 1..columns) {
+                        c.set(i, j, c.get(i, j) + get(i, k) * b.get(k, j))
+                    }
+            return c
+        } else
+            throw IllegalArgumentException("Matrix multiplication error")
+    }
+
     operator fun times(b: Double): Matrix {
         val c = Matrix(rows, columns)
         for (i in matrix.indices)
@@ -51,5 +64,18 @@ class Matrix(val rows: Int, val columns: Int) {
         for (i in matrix.indices)
             c.matrix[i] = this * b.matrix[i]
         return c
+    }
+
+    override fun toString(): String {
+        var s = String()
+        s += '\n'
+        for (i in 1..rows) {
+            for (j in 1..columns) {
+                s += get(i, j)
+                s += ' '
+            }
+            s += '\n'
+        }
+        return s
     }
 }
